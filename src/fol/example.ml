0e72ac736be2b95668e14227d73bdef09fceca6c
Theophilos Giannakopoulos <tgiannak@alum.wpi.edu>
2011-07-06 17:17:03 -0400

    Improved example for FOL library a little bit.

--- src/fol/example.ml
+++ src/fol/example.ml
@@ -1,5 +1,6 @@
 open SortedFol
 
 let _ =
-  let thy = string2theory "(exists y A (y = c))" in
-  Syntax.output_formulas stdout thy
+  let thy = string2theory "(exists y A (y = c)) (forall b B (not (r b)))" in
+  Syntax.output_formulas stdout thy;
+  print_endline "" 
